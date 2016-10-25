1c381193466a2c65995b4fbba8de728c5fdb3b14
quentinella14 <quentinella14@hotmail.fr>
2016-10-25 20:48:02 +0200

    Add Project

---
+++ primenumbers/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace primenumbers
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			DateTime starttime = DateTime.Now;
+			int max = 1000000;
+			int b = 2;
+			for (int i = 1; i < max; i += b) {
+				int c = 0;
+				for (int j = 1; j != Math.Round(Math.Sqrt(i)) + 1; i++) {
+					if (i % j == 0) {
+						c++;
+					}
+					if (b > 1) {
+						break;
+					}
+				}
+				//if (c == 1) {
+					//Console.WriteLine (i);
+				//}
+				b = 6 - b;
+			}
+			TimeSpan tempsecoule = DateTime.Now - starttime;
+			Console.WriteLine (tempsecoule);
+		}
+	}
+}
