48f79bafec57bc3613b9cfbe2f1a4339ecf22451
quentinella14 <quentinella14@hotmail.fr>
2016-10-26 15:42:25 +0200

    Improvement

--- primenumbers/Program.cs
+++ primenumbers/Program.cs
@@ -11,17 +11,13 @@ namespace primenumbers
 			int b = 2;
 			for (int i = 1; i < max; i += b) {
 				int c = 0;
-				for (int j = 1; j != Math.Round(Math.Sqrt(i)) + 1; j++) {
+				for (int j = 2; j != Math.Round(Math.Sqrt(i)) + 1; j++) {
 					if (i % j == 0) {
 						c++;
-					}
-					if (c > 1) {
 						break;
 					}
 				}
-				//if (c == 1) {
-				//	Console.WriteLine (i);
-				//}
+				//if (c == 0) Console.WriteLine (i);
 				b = 6 - b;
 			}
 			TimeSpan tempsecoule = DateTime.Now - starttime;
